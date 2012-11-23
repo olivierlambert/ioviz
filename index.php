d33b6c3883772d174a26e1b6399cd12581dfd384
Olivier Lambert <olivier.lambert@vates.fr>
2012-11-23 13:25:08 +0100

    Big modification : IOViz is now Object oriented

--- index.php
+++ index.php
@@ -10,29 +10,14 @@
 <?php
 require_once("ioviz.php");
 
-// load all DATA JS
-$handle=opendir("data/");
-while (($file = readdir($handle))!==false) {
-	if (strstr($file,".js")) {
-	echo '<script type="text/javascript" src="data/'.$file.'"></script>';
-	}
-}
-closedir($handle);
-//
 
-// to do : browse to upload file then load bench by hosts (subfolders)
-$iozone = parse_data("/data/resultth.txt");
-$iozone2 = parse_data("/data/resultlocal.txt");
 
-$bench_name= "write";
-$bench_name2= "writelocal";
-$graph_data = create_graph_data($bench_name,$iozone[1]);
-$graph_data2 = create_graph_data($bench_name2,$iozone2[1]);
-$tick_data = tick_generator($iozone[0]);
-$tick_data2 = tick_generator($iozone2[0]);
+// filename, bench_name, description
+$ioviz = new Ioviz("/data/resultth.txt","ZFS","Zfs bench on NFS Linux Client");
 
-echo draw_graph($tick_data,$graph_data,$bench_name,"Zfs on NFS Linux client","WRITER Perfs in Mbytes/s");
-echo draw_graph($tick_data2,$graph_data2,$bench_name2,"Ext4 on local disk","WRITER Perfs in Mbytes/s");
+$ioviz->get_graph();
+
+
 ?>
 
 
